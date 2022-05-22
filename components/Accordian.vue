9074b2182384f2b14286da3f3eb8a0db3b40de18
thepenielcho <mail@penielcho.com>
2022-05-23 01:50:46 +0900

    accordian font size - mediaquery

--- components/Accordian.vue
+++ components/Accordian.vue
@@ -10,7 +10,7 @@
       </button>
     </h2>
     <div id="collapseOne5" class="accordion-collapse collapse show" aria-labelledby="headingOne5">
-      <div class="accordion-body py-6 px-5 text-sm">
+      <div class="accordion-body py-6 px-5 text-sm md:text-base">
         참여 자격은 따로 존재하지 않습니다! GDSC 중앙대 챕터에 관심이 있는 분이라면 누구든 참관하실 수 있어요. GDSC 멤버이실 필요도, 중앙대생이실 필요도, 대학생이실 필요도 없어요.
       </div>
     </div>
@@ -38,7 +38,7 @@
       </button>
     </h2>
     <div id="collapseTwo5" class="accordion-collapse collapse" aria-labelledby="headingTwo5">
-      <div class="accordion-body py-6 px-5 text-sm">
+      <div class="accordion-body py-6 px-5 text-sm md:text-base">
         6월 28일부터 Festa 플랫폼을 통해 무료 티켓을 판매해요. 티켓을 구매하시면 타임테이블, 컨퍼런스 장소 등 자세한 안내 메일을 보내드릴 예정이에요.
       </div>
     </div>
@@ -66,7 +66,7 @@
       </button>
     </h2>
     <div id="collapseThree5" class="accordion-collapse collapse" aria-labelledby="headingThree5">
-      <div class="accordion-body py-6 px-5 text-sm">
+      <div class="accordion-body py-6 px-5 text-sm md:text-base">
         GDSC CAUCON 2022는 GDSC 중앙대 챕터의 활동보고 세션과 메타(Meta) 소프트웨어 엔지니어의 키노트, GDSC 중앙대 챕터 멤버들의 프로젝트 발표 세션들, GDSC 중앙대 2기 모집 설명 세션으로 구성되어 있어요.
       </div>
     </div>
