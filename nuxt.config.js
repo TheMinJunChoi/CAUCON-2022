7c81459aeace959040bbf00b7ab6151e8f066c90
Peniel Cho | 조용주 <mail@penielcho.com>
2022-07-05 01:11:21 +0900

    [chore] Router Base Link 수정
    gh-pages 하위 레포 배포 위한 라우팅 베이스 변경 및 패키지 URL 변경

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/CAUCON/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
